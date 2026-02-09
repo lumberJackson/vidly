97d244c83a0e2eef9c359e7811e1668cfe66102b
lumberJackson <38990788+lumberJackson@users.noreply.github.com>
2026-02-08 21:14:29 -0600

    added index function in movie controller

--- vidly/Controllers/MovieController.cs
+++ vidly/Controllers/MovieController.cs
@@ -10,4 +10,16 @@ public class MovieController : Controller
         var movie = new Movie() { Name = "Shrek" };
         return View(movie);
     }
+
+    public IActionResult Edit(int id)
+    {
+        return Content("Index: " + id);
+    }
+
+    public IActionResult Index(int? pageIndex, string sortby)
+    {
+        if (pageIndex == null) pageIndex = 1;
+        if (string.IsNullOrEmpty(sortby)) sortby = "name";
+        return Content(string.Format("PageIndex: {0}, SortBy: {1}", pageIndex, sortby));
+    }
 }
